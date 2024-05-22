10206b7c163779463ba89afce1fac2d4d86a6575
Dani Ibarra <dibarra19976@iesjoanramis.org>
2024-05-22 12:49:11 +0200

    img fix 1

--- src/components/primary/BoxComponent.jsx
+++ src/components/primary/BoxComponent.jsx
@@ -2,7 +2,7 @@ export const BoxComponent = ({ boxObj }) => {
   console.log(boxObj);
   return (
     <div className="box">
-      <img src={`/src/assets/${boxObj.img}`} alt="" className="boxImg" />
+      <img src={`./${boxObj.img}`} alt="" className="boxImg" />
       <h1>{boxObj.name}</h1>
       <span className="info">
         <span className="label">Owner:</span> {boxObj.owner}
